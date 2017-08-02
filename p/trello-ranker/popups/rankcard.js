8f61e291ba9d07f02984f45667ce77c4f44bb6e1
Jobo Danque <jmdanque@jmdanque.smi.com.ph>
2017-08-03 00:01:21 +0800

    added func to rank popup

---
+++ p/trello-ranker/popups/rankcard.js
@@ -0,0 +1,17 @@
+var t = TrelloPowerUp.iframe();
+
+window.rankcard.addEventListener('submit',function(ev){
+	ev.preventDefault();
+	return t.set('card','shared','rank', window.rankcurrent.value)
+	.then(function(){
+		t.closePopup();
+	});
+});
+
+window.rankup.addEventListener('click',function(ev){
+	window.rankcurrent.value += 1;
+});
+
+window.rankdown.addEventListener('click',function(ev){
+	window.rankcurrent.value -= 1;
+});

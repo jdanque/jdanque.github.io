94a1ea3c9a5af240f0ee795f766e8ee155e9404a
Jobo Danque <jmdanque@jmdanque.smi.com.ph>
2017-08-03 00:33:29 +0800

    fixes and added card-detail-badges

--- p/trello-ranker/js/m.js
+++ p/trello-ranker/js/m.js
@@ -25,9 +25,19 @@ TrelloPowerUp.initialize({
       .then(function(rank) {
         return [{
           icon: 'https://cdn.glitch.com/c69415fd-f70e-4e03-b43b-98b8960cd616%2Frocket-ship-grey.png?1496162964717',
-          text: rank || 0
+          text: rank || '0'
         }];
      });
   },
+	'card-detail-badges': function(t, options) {
+	  return t.get('card', 'shared', 'rank')
+	  .then(function(rank) {
+	    return [{
+	      title: 'Rank',
+	      text: rank || '0',
+	      callback: cardButtonCallback
+	    }]
+	  });
+	}
 
 });

9451cee14d3f5f5bd98fe1f1f55630641bf1d614
Jobo Danque <jmdanque@jmdanque.smi.com.ph>
2017-08-03 14:09:13 +0800

    test auth

--- p/trello-ranker/auth/js/auth.js
+++ p/trello-ranker/auth/js/auth.js
@@ -8,7 +8,7 @@ var KEY = 'a6394dd0e3edbb531ad4d143c6a11ed5a0c3dd466c4e79ed2a62f8ca405ac99b';
 // APPNAME and RETURNURL
 // You can do that with the encodeURIComponent(string) function
 // encodeURIComponent('Hello World') -> "Hello%20World"
-var oauthUrl = 'https://trello.com/1/authorize?expiration=never&name=SinglePurposeToken&key='+KEY;
+var oauthUrl = 'https://trello.com/1/authorize?callback_method='+KEY;
 
 var tokenLooksValid = function(token) {
   return /^[0-9a-f]{64}$/.test(token);
@@ -20,19 +20,42 @@ var authorizeOpts = {
   validToken: tokenLooksValid
 };
 
+
+
+var authenticationSuccess = function() {
+  window.content.value='Successful authentication';
+};
+
+var authenticationFailure = function() {
+  window.content.value='Failed authentication';
+};
+
+
 var authBtn = document.getElementById('authorize');
 authBtn.addEventListener('click', function() {
-  t.authorize(oauthUrl, authorizeOpts)
-  .then(function(token) {
-    return t.set('organization', 'private', 'token', token)
-    .catch(t.NotHandled, function() {
-      // fall back to storing at board level
-      return t.set('board', 'private', 'token', token);
-    });
-  })
-  .then(function() {
-    // now that the token is stored, we can close this popup
-    // you might alternatively choose to open a new popup
-    return t.closePopup();
-  });
+
+	window.Trello.authorize({
+	  type: 'popup',
+	  name: 'Trello Ranker',
+	  scope: {
+	    read: 'true',
+	    write: 'true' },
+	  expiration: 'never',
+	  success: authenticationSuccess,
+	  error: authenticationFailure
+	});
+
+//  t.authorize(oauthUrl, authorizeOpts)
+//  .then(function(token) {
+//    return t.set('organization', 'private', 'token', token)
+//    .catch(t.NotHandled, function() {
+//      // fall back to storing at board level
+//      return t.set('board', 'private', 'token', token);
+//    });
+//  })
+//  .then(function() {
+//    // now that the token is stored, we can close this popup
+//    // you might alternatively choose to open a new popup
+//    return t.closePopup();
+//  });
 });

1ae0583e28846e7a462eebb009318332a5824ae2
Jobo Danque <jmdanque@jmdanque.smi.com.ph>
2017-08-03 14:26:41 +0800

    test auth

--- p/trello-ranker/auth/js/auth.js
+++ p/trello-ranker/auth/js/auth.js
@@ -8,7 +8,14 @@ var KEY = 'e3e4df7f95e0b1942c0b82a9a2c301f6';
 // APPNAME and RETURNURL
 // You can do that with the encodeURIComponent(string) function
 // encodeURIComponent('Hello World') -> "Hello%20World"
-var oauthUrl = 'https://trello.com/1/authorize?callback_method='+KEY;
+var oauthUrl = 'https://trello.com/1/authorize?+
+'callback_method=postMessage'+'&'+
+'return_url='+encodeURIComponent('https://jdanque.github.io')+'&'+
+'scope=read,write'+'&'+
+'expiration=never'+'&'+
+'name='+encodeURIComponent(APP_NAME)+'&'+
+'key'+KEY+
+'';
 
 var tokenLooksValid = function(token) {
   return /^[0-9a-f]{64}$/.test(token);
@@ -17,9 +24,8 @@ var tokenLooksValid = function(token) {
 var authorizeOpts = {
   height: 680,
   width: 580,
-  validToken: tokenLooksValid
+  validToken: KEY
 };
-
 
 
 var authenticationSuccess = function() {
@@ -34,29 +40,29 @@ var authenticationFailure = function() {
 var authBtn = document.getElementById('authorize');
 authBtn.addEventListener('click', function() {
 
-	window.Trello.authorize({
-	  key : KEY,
-	  type: 'popup',
-	  name: 'Trello Ranker',
-	  scope: {
-	    read: 'true',
-	    write: 'true' },
-	  expiration: 'never',
-	  success: authenticationSuccess,
-	  error: authenticationFailure
-	});
+//	window.Trello.authorize({
+//	  key : KEY,
+//	  type: 'popup',
+//	  name: 'Trello Ranker',
+//	  scope: {
+//	    read: 'true',
+//	    write: 'true' },
+//	  expiration: 'never',
+//	  success: authenticationSuccess,
+//	  error: authenticationFailure
+//	});
 
-//  t.authorize(oauthUrl, authorizeOpts)
-//  .then(function(token) {
-//    return t.set('organization', 'private', 'token', token)
-//    .catch(t.NotHandled, function() {
-//      // fall back to storing at board level
-//      return t.set('board', 'private', 'token', token);
-//    });
-//  })
-//  .then(function() {
-//    // now that the token is stored, we can close this popup
-//    // you might alternatively choose to open a new popup
-//    return t.closePopup();
-//  });
+  t.authorize(oauthUrl, authorizeOpts)
+  .then(function(token) {
+    return t.set('organization', 'private', 'token', token)
+    .catch(t.NotHandled, function() {
+      // fall back to storing at board level
+      return t.set('board', 'private', 'token', token);
+    });
+  })
+  .then(function() {
+    // now that the token is stored, we can close this popup
+    // you might alternatively choose to open a new popup
+    return t.closePopup();
+  });
 });

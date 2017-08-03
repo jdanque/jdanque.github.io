164c4d9c9e261355fe6d814ab26e6aebd481c6a3
Jobo Danque <jmdanque@jmdanque.smi.com.ph>
2017-08-04 03:43:06 +0800

    please make it to the deadline

---
+++ p/trello-tree-view/auth/auth.js
@@ -0,0 +1,18 @@
+var Promise = TrelloPowerUp.Promise;
+var t = TrelloPowerUp.iframe();
+var r_url = encodeURIComponent('https://jdanque.github.io/p/trello-tree-view/auth/auth-success.html');
+var trelloAuthUrl = 'https://trello.com/1/authorize?expiration=never&name=Tree%20View%20for%20Trello&scope=read,write&key=e3e4df7f95e0b1942c0b82a9a2c301f6&callback_method=fragment&return_url='+r_url;
+
+var tokenLooksValid = function(token) {
+	return /^[0-9a-f]{64}$/.test(token);
+}
+
+document.getElementById('auth-btn').addEventListener('click', function(){
+	t.authorize(trelloAuthUrl, { height: 680, width: 580, validToken: tokenLooksValid })
+		.then(function(token){
+			return t.set('member', 'private', 'token', token)
+		})
+		.then(function(){
+			return t.closePopup();
+		});
+});
---
+++ p/trello-tree-view/main.js
@@ -0,0 +1,41 @@
+var API_KEY = 'e3e4df7f95e0b1942c0b82a9a2c301f6';
+var ICON = './img/treeviewlogo.svg';
+
+var openTreeViewOverlay = function (t, opts) {
+  return t.overlay({
+    url: './overlay/treeview.html',
+    args: {} // optional args to pass
+  });
+};
+
+TrelloPowerUp.initialize({
+
+	'board-buttons': function (t, opts) {
+		return [{
+			icon: ICON,
+			text: 'Trello Tree View',
+			callback: openTreeViewOverlay
+		}];
+	},
+	'authorization-status': function(t, options){
+		return t.get('member', 'private', 'token')
+			.then(function(authToken) {
+				return { authorized: authToken != null }
+			});
+	},
+	'show-authorization': function(t, options){
+		return t.popup({
+			title: 'Authorize Account',
+			url: './auth/auth.html',
+			height: 140,
+		});
+	},
+	'show-settings': function(t, options){
+		return t.popup({
+			title: 'Settings',
+			url: './settings/settings.html',
+			height: 184
+		});
+	}
+
+});

8ca52bad1eb72662b7316cc18b5a126d7e8d3f7a
Jobo Danque <jmdanque@jmdanque.smi.com.ph>
2017-08-22 08:25:17 +0800

    moved to cloud

--- p/trello-tree-view/js/main.js
+++ p/trello-tree-view/js/main.js
@@ -1,13 +1,9 @@
 var API_KEY = 'e3e4df7f95e0b1942c0b82a9a2c301f6';
-//var ICON = 'https://d1ipfkx2xm6eh3.cloudfront.net/img/logo.svg';
-//todo
-var ICON = './img/logo_white.svg';
+var ICON = 'https://d1ipfkx2xm6eh3.cloudfront.net/img/logo_white.svg?v=1.0.0';
 
 var openTreeViewOverlay = function (t, opts) {
   return t.overlay({
-//    url: 'https://d1ipfkx2xm6eh3.cloudfront.net/view/treeview.html',
-	//todo
-    url: './view/treeview.html',
+    url: 'https://d1ipfkx2xm6eh3.cloudfront.net/view/treeview.html?v=1.0.0'
 //    args: {mainT : t} // optional args to pass
   });
 };
@@ -30,17 +26,14 @@ TrelloPowerUp.initialize({
 	'show-authorization': function(t, options){
 		return t.popup({
 			title: 'Authorize Account',
-			//todo
-			url: './view/auth.html',
+			url: 'https://d1ipfkx2xm6eh3.cloudfront.net/view/auth.html?v=1.0.0',
 			height: 140,
 		});
 	},
 	'show-settings': function(t, options){
 		return t.popup({
 			title: 'TreeView Settings',
-//			url: 'https://d1ipfkx2xm6eh3.cloudfront.net/view/settings.html',
-//todo
-			url: './view/settings.html',
+			url: 'https://d1ipfkx2xm6eh3.cloudfront.net/view/settings.html?v=1.0.0',
 			height: 184
 		});
 	}

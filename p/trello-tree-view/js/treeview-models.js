8710759097e25b4ddabc62d90fac99e8b6c5f2a3
Jobo Danque <jmdanque@jmdanque.smi.com.ph>
2017-08-26 01:18:02 +0800

    testing some stuff

--- p/trello-tree-view/js/treeview-models.js
+++ p/trello-tree-view/js/treeview-models.js
@@ -32,7 +32,7 @@ TreeView.Models.Boards = Backbone.Collection.extend({
 
 TreeView.Models.List = TreeView.Models.Node.extend({
 	defaults :  _.extend({},TreeView.Models.Node.prototype.defaults,{
-		'type'  : 'list'
+		'type'  : 'list',
 		'cards': []
 	}),
 	initialize : function(){

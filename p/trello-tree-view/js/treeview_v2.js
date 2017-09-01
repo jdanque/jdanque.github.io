bdf10a919df140d86cfb3920dee9d8f92c0e7610
Jobo Danque <jmdanque@jmdanque.smi.com.ph>
2017-09-01 17:08:50 +0800

    :construction: dragand drop

--- p/trello-tree-view/js/treeview_v2.js
+++ p/trello-tree-view/js/treeview_v2.js
@@ -305,7 +305,7 @@ Backbone.Collection.prototype.move = function(model, toIndex) {
 					me._models.main.get('subnodes').at(0)
 						.get('subnodes').findWhere({'id': d.newList})
 						.get('subnodes')
-						.add(cardModel,{at: cardsInList.index(card) , silent : true});
+						.add(cardModel,{at: cardsInList.index(card), noAppend : true});
 
 				  }
 

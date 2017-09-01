991a0f685788290f83a0509774c8e73aa2dcc9c6
Jobo Danque <jmdanque@jmdanque.smi.com.ph>
2017-09-01 12:43:39 +0800

    :construction: moving to t.modal

--- p/trello-tree-view/js/main.js
+++ p/trello-tree-view/js/main.js
@@ -12,7 +12,7 @@ var openTreeViewOverlay = function (t, opts) {
 		url: 'https://jdanque.github.io/p/trello-tree-view/view/treeview.html?v=1.0.0',
 		fullscreen : false,
 		title: 'TreeView for Trello',
-		height: 600,
+		height: 300,
 		accentColor : _accentColor
 	  });
 	});
